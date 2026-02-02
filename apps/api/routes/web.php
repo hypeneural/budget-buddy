51e589f909190f2e30c921a3507e326ef4e83168
Hype Neural <hypeneural@gmail.com>
2026-02-02 13:05:43 -0300

    v020226-1305

--- apps/api/routes/web.php
+++ apps/api/routes/web.php
@@ -3,12 +3,15 @@
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
+// Redirect root to the SPA
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/app/');
 });
 
+// Serve the SPA for all /app/* routes
 Route::get('/app/{any?}', function () {
     $path = public_path('app/index.html');
     abort_unless(File::exists($path), 404);
     return File::get($path);
 })->where('any', '.*');
+
